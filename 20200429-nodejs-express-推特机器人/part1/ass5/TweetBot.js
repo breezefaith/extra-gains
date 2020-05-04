baba7ab847a0e24c3774678ec3573680e165c316
zhangzhichao02 <zhangzhichao02@inspur.com>
2020-05-05 00:57:58 +0800

    backup

--- 20200429-nodejs-express-推特机器人/part1/ass5/TweetBot.js
+++ 20200429-nodejs-express-推特机器人/part1/ass5/TweetBot.js
@@ -3,8 +3,26 @@ const express = require('express');
 const server = express();
 
 var client = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    consumer_key: 'WC3HvtIQdLbE99rEFK1N0SCSG',
+    consumer_secret: 'OhqIdzEmC80FuujUNaLNr48NkUF20xchSAfkSgycxPyhUlILW6',
+    access_token_key: '1255635765842915330-qSH4qGwJNBQnd8d4ZluDDl2zkwVLbV',
+    access_token_secret: 'xNr5QEFO7unfexUVHogWqO3sgOrreR7yOLkaN6DRP43m2'
 });
+
+server.get("/api/tweets/:param", function (req, res, next) {
+    var param = req.params.param;
+    console.log(param);
+    client.get("search/tweets", { q: param, count: 50 }, function (error, tweets, response) {
+        console.error(error);
+        console.log(tweets);
+        console.log(response);
+        res.json(tweets).end();
+    })
+});
+
+var host = server.listen(5001, function () {
+    var addr = host.address().address;
+    var port = host.address().port;
+
+    console.log("serve on http://%s:%s", addr, port);
+})
