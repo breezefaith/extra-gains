5ae95770d8726de8abb2f74ee4d8a71411fdd494
zzcoder <nyzhangzc@qq.com>
2020-05-05 20:38:41 +0800

    20200429-nodejs-express-推特机器人完成

--- 20200429-nodejs-express-推特机器人/part1/ass5/TweetBot.js
+++ 20200429-nodejs-express-推特机器人/part1/ass5/TweetBot.js
@@ -1,8 +1,15 @@
-var Twitter = require('twitter');
+const Twitter = require('twitter');
 const express = require('express');
-var Sentiment = require('sentiment');
-var sentiment = new Sentiment();
 const server = express();
+
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
+const language = "EN"
+const defaultCategory = 'N';
+const defaultCategoryCapitalized = 'NNP';
+var lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapitalized);
+var ruleSet = new natural.RuleSet('EN');
+var tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
 
 var fs = require("fs");
 var tweets = JSON.parse(fs.readFileSync('part1.txt').toString());
@@ -15,51 +22,164 @@ var client = new Twitter({
 });
 
 server.get("/api/tweets/:param", function (req, res, next) {
-    // var param = req.params.param;
-    // client.get("search/tweets", { q: param, count: 50 }, function (error, tweets, response) {
-    //     if(!error){
-    //         console.error(error);
-    //         res.end();
-    //     }else{
-    //         tweets.statuses.forEach(element => {
-    //             console.log(element.text);
-    //         });
-    //         res.json(tweets).end();
-    //     }
-    // })
-    var texts = tweets.statuses.map(elt => elt.text);
-    res.json(basic(req, res, texts)).end();
+    var param = req.params.param;
+    client.get("search/tweets", { q: param, count: 50 }, function (error, tweets, response) {
+        if(!error){
+            console.error(error);
+            res.end();
+        }else{
+            var texts = tweets.statuses.map(elt => elt.text);
+            res.write(basic(texts));
+            // res.write("\n");
+            // res.write(intermediate(texts));
+            // res.write("\n");
+            // res.write(harder(texts));
+            res.end();
+        }
+    })
 });
 
-function basic(req, res, texts) {
-    var result = {};
+function basic(texts) {
+    function getWordCount(words) {
+        var obj = {};
+        for (var i = 0, l = words.length; i < l; i++) {
+            var item = words[i];
+            obj[item] = (obj[item] + 1) || 1;
+        }
+        return obj;
+    }
+
+    function findWord(wordList, key) {
+        for (const obj of wordList) {
+            if (obj.key == key) {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    var table = [];
     texts.forEach(txt => {
-        // console.log(txt);
-        // txt.split("[\\p{Punct}\\s]+").forEach(word=>{
-        // txt.replace("[\\p{Punct}\\s]+","#tweetbot#").split("#tweetbot#").forEach(word => {
-            // console.log(word);
-            // if (result[word] == null) {
-            //     result[word] = 1;
-            // } else {
-            //     result[word]++;
-            // }
-        // });
-        var resu= sentiment.analyze(txt);
-        console.dir(resu);
+        list = tokenizer.tokenize(txt);
+        var tmp = getWordCount(list);
+        for (const key in tmp) {
+            var obj = findWord(table, key);
+            if (obj == null) {
+                table.push({ key: key, count: tmp[key] });
+            } else {
+                obj.count += tmp[key];
+            }
+        }
     });
-    return result;
-    // res.end();
+    table.sort((a, b) => b.count - a.count);
+
+    var res = "";
+    for (const item of table) {
+        res += item.key + " ";
+        if (res.length >= 280) {
+            res = res.substr(0, 279);
+            break;
+        }
+    }
+    res += ".";
+    return res;
 }
 
 function intermediate(texts) {
+    function getTaggedWordCount(taggedWords) {
+        var obj = {};
+        for (var i = 0, l = taggedWords.length; i < l; i++) {
+            var item = taggedWords[i].token;
+            obj[item] = obj[item] || {};
+            obj[item].count = (obj[item].count + 1) || 1;
+            obj[item].tag = taggedWords[i].tag;
+        }
+        return obj;
+    }
 
+    function findWord(wordList, key) {
+        for (const obj of wordList) {
+            if (obj.key == key) {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    var table = {
+        article: [],
+        adj: [],
+        noun: [],
+        verb: [],
+        preposition: [],
+        conjunction: [],
+        adv: [],
+        pronoun: []
+    };
+    texts.forEach(txt => {
+        var taggedWords = tagger.tag(tokenizer.tokenize(txt)).taggedWords;
+        var tmp = getTaggedWordCount(taggedWords);
+
+        for (const key in tmp) {
+            var obj;
+            var tag;
+
+            if (tmp[key].tag.startsWith("VB")) {
+                obj = findWord(table.verb, key);
+                tag = "verb";
+            } else if (tmp[key].tag.startsWith("N")) {
+                obj = findWord(table.noun, key);
+                tag = "noun";
+            } else if (tmp[key].tag.startsWith("DT")) {
+                obj = findWord(table.article, key);
+                tag = "article";
+            } else if (tmp[key].tag.startsWith("CC")) {
+                obj = findWord(table.conjunction, key);
+                tag = "conjunction";
+            } else if (tmp[key].tag.startsWith("JJ")) {
+                obj = findWord(table.adj, key);
+                tag = "adj";
+            } else if (tmp[key].tag.startsWith("IN")) {
+                obj = findWord(table.preposition, key);
+                tag = "preposition";
+            } else if (tmp[key].tag.startsWith("PRP")) {
+                obj = findWord(table.pronoun, key);
+                tag = "pronoun";
+            } else if (tmp[key].tag.startsWith("RB")) {
+                obj = findWord(table.adv, key);
+                tag = "adv";
+            } else {
+                tag = null;
+                obj = null;
+            }
+            if (tag != null) {
+                if (obj == null) {
+                    table[tag].push({ key: key, count: tmp[key].count });
+                } else {
+                    obj.count += tmp[key].count;
+                }
+            }
+        }
+    });
+
+    for (const key in table) {
+        if (table.hasOwnProperty(key)) {
+            table[key].sort((a, b) => b.count - a.count);
+        }
+    }
+
+    var res = "";
+    res += `${table.article[0].key} ${table.adj[0].key}, ${table.adj[1].key} ${table.noun[0].key} ${table.verb[0].key} ${table.preposition[0].key} ${table.article[1].key} ${table.noun[1].key} `;
+    res += `${table.conjunction[0].key} ${table.adv[0].key} ${table.verb[1].key} ${table.preposition[1].key} ${table.pronoun[0].key} ${table.noun[2].key}.`;
+
+    return res;
 }
 
 function harder(texts) {
-
+    return intermediate(texts);
 }
 
-var host = server.listen(5001, function () {
+var host = server.listen(5000, function () {
     var addr = host.address().address;
     var port = host.address().port;
 
