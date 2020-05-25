fb5447e0e268a74f786018a70ceb796a979c360c
zhangzhichao02 <zhangzhichao02@inspur.com>
2020-05-25 15:50:52 +0800

    backup

---
+++ Scaffold/angular/angular-scaffold/src/app/manager/entity/operation-type.enum.ts
@@ -0,0 +1,5 @@
+export enum OperationType {
+    Create,
+    Edit,
+    View
+}
--- Scaffold/angular/angular-scaffold/src/app/manager/manager.routing.ts
+++ Scaffold/angular/angular-scaffold/src/app/manager/manager.routing.ts
@@ -6,7 +6,7 @@ import { DetailComponent } from './detail/detail.component';
 const routes: Routes = [
     { path: "", redirectTo: "aif-biz-action-list", pathMatch: "full" },
     { path: "list", component: ListComponent, },
-    { path: "detail/create", component: DetailComponent, },
+    { path: "detail", component: DetailComponent, },
     { path: "detail/:id", component: DetailComponent, },
 ];
 
