7383fbde9c02869505de676a8b4a3d76c487cf73
zhangzhichao02 <zhangzhichao02@inspur.com>
2020-07-09 15:51:32 +0800

    backup

---
+++ Scaffold/javafx/ms-base/src/base/manager/system/entity/AbstractEntity.java
@@ -0,0 +1,13 @@
+package base.manager.system.entity;
+
+public class AbstractEntity {
+    protected String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ Scaffold/javafx/ms-base/src/base/manager/system/service/impl/AbstractEntityService.java
@@ -0,0 +1,7 @@
+package base.manager.system.service.impl;
+
+import base.manager.system.entity.AbstractEntity;
+import base.manager.system.service.IEntityService;
+
+public abstract class AbstractEntityService<T extends AbstractEntity> implements IEntityService<T> {
+}
