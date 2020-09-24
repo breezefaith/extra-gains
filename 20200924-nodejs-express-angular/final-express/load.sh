569e14a999a5162107b12b8f07bc0fae225a58e2
zzcoder <nyzhangzc@qq.com>
2020-09-24 20:54:30 +0800

    backup

---
+++ 20200924-nodejs-express-angular/final-express/load.sh
@@ -0,0 +1,3 @@
+# put your code for loading student data to mongodb
+use final;
+db.Students.insert([{"sid":123456789,"name":"John Cho","dept":"Computer Science","title":"Designer"},{"sid":234567890,"name":"Megan Fox","dept":"Residential Life","title":"Coder"}]);
